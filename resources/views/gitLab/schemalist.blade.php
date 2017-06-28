a3999df4d99f1f30a781eaf9c83b98d62eefff76
Rolf Guescini <rolf.guescini@hu-berlin.de>
2017-06-28 13:59:50 +0200

    Cleaning up remnants from Auth0-code

--- resources/views/gitLab/schemalist.blade.php
+++ resources/views/gitLab/schemalist.blade.php
@@ -7,19 +7,17 @@
             @foreach($schema as $schemafile)
                 @if($schemafile["type"] == "dir")
                     <li><a href="{{ route('gitRepo.route.schema', array('path' => $schemafile['path'])) }}"> {{$schemafile['basename']}}</a>
-                        @if($pathcount == 2 && in_array("KM",$user['roles']) || $pathcount == 1 && in_array("Admin",$user['groups']))
+                        @if($pathcount == 2 || $pathcount == 1)
                             | <a href="{{route('gitRepo.deleteFile.route', array('path' => $schemafile['path'], 'isdir' => 1)) }}">Delete</a>
                         @endif
 
-                        @if($schemafile["tracked"] == "false" && $schemafile["foldercount"] > 0 && $pathcount == 3  && (in_array("Admin",$user['groups'])))
+                        @if($schemafile["tracked"] == "false" && $schemafile["foldercount"] > 0 && $pathcount == 3 )
                             | <a href="{{route('gitRepo.addFile.route', array('path' => $schemafile['path'])) }}">Add to GIT</a>
                         @endif
                     </li>
                 @else
                     <li><a href="{{ route('gitRepo.readFile.route', array('path' => $schemafile['path'])) }}"> {{$schemafile['basename']}}</a>
-                        @if( in_array("KM",$user['roles']) || in_array("Admin",$user['groups']))
                             | <a href="{{route('gitRepo.deleteFile.route', array('path' => $schemafile['path'], 'isdir' => 0)) }}">Delete</a>
-                        @endif
                     </li>
                 @endif
 
@@ -27,14 +25,14 @@
         </ul>
 
 
-        @if($pathcount == 1 && $path == "" && in_array("GKA",$user['roles']))
+        @if($pathcount == 1 && $path == "")
             <a href="{{route('gitRepo.createproject.get',array('dirname' => $schemafile['dirname'])) }}" >Create project</a>
         @endif
 
 
     @else
 
-        @if($pathcount == 1 && in_array("Admin",$user['groups']))
+        @if($pathcount == 1)
             <a href="{{route('gitRepo.upload.get',array('dirname' => $path)) }}" >Upload files</a>
         @endif
     @endif
