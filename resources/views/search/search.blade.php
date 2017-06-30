421ba22ecb2e04f1851773559470b954850102b3
Rolf Guescini <rolf.guescini@hu-berlin.de>
2017-06-30 10:40:12 +0200

    Implementing Holy-Grail flex layout

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -1,9 +1,28 @@
-@extends('layouts.search')
+@extends('layouts.flexsearch')
 
 @section('content')
-    <div id="searchapp">
-        <h1>Search</h1>
-        <searchpanel_general v-on:searchedgeneral="askElastic"></searchpanel_general>
-        <searchwrapper :results="results"></searchwrapper>
+    <div id="searchapp" class="HolyGrail">
+        <header class="HolyGrail-header">
+            <div class="Header Header--cozy" role="banner">
+                <searchpanel_general v-on:searchedgeneral="askElastic"></searchpanel_general>
+            </div>
+
+        </header>
+        <main class="HolyGrail-body">
+            <article class="HolyGrail-content">
+                <searchwrapper :results="results"></searchwrapper>
+            </article>
+            <nav class="HolyGrail-nav u-textCenter">
+                <strong>Boxes</strong>
+            </nav>
+            <aside class="HolyGrail-filters u-textCenter">
+                <strong>Filters</strong>
+            </aside>
+        </main>
+        <footer class="HolyGrail-footer">
+            <div class="Footer">
+                Footer
+            </div>
+        </footer>
     </div>
 @stop
