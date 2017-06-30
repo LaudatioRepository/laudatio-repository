ee4342b7ca3839d7673760b9300921361dca58fe
Rolf Guescini <rolf.guescini@hu-berlin.de>
2017-06-30 11:14:53 +0200

    Adding deploy script and node_modules for deploy

---
+++ deploy.php
@@ -0,0 +1,56 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Configuration
+
+set('repository', 'git@bitbucket.org:guescini-HU/vuelasticsearchjslaudatio.git');
+set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+add('shared_files', ['.env','config/flysystem.php','config/filesystems.php','config/gitlab.php']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', []);
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+set('http_user', 'www-data');
+set('permission_method', 'chmod_777');
+set('writable_use_sudo', true);
+set('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/framework/views/gitLab',
+    'storage/logs',]);
+
+
+// Hosts
+host('depot1-5.cms.hu-berlin.de')
+    ->user('root')
+    ->identityFile('/Users/rolfguescini/.ssh/id_rsa.pub', '/Users/rolfguescini/.ssh/id_rsa', '')
+    ->set('deploy_path', '/var/www/html/laravelaudatio')
+    ->set('branch', 'phpelastic')
+    ->user('root');
+
+
+// Tasks
+task('environment', function () {
+    run('cp /var/www/html/laravelaudatio/flysystem.php {{release_path}}/config/flysystem.php');
+    run('cp /var/www/html/laravelaudatio/filesystems.php {{release_path}}/config/filesystems.php');
+    run('cp /var/www/html/laravelaudatio/gitlab.php {{release_path}}/config/gitlab.php');
+    run('cp /var/www/html/laravelaudatio/laudatio.php {{release_path}}/config/laudatio.php');
+})->desc('Environment setup');
+
+
+desc('Execute artisan config:clear');
+task('clearconfig', function () {
+    run('{{bin/php}} {{release_path}}/artisan config:clear');
+});
+
+after('deploy','environment');
+after('deploy','clearconfig');
+after('deploy:failed', 'deploy:unlock');
