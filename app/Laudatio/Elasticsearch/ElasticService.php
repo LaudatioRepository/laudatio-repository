80a6d4738ebe10cc297fb9442416d319da6d1e08
Rolf Guescini <rolf.guescini@hu-berlin.de>
2017-10-13 11:24:40 +0200

    metrics cont..

--- app/Laudatio/Elasticsearch/ElasticService.php
+++ app/Laudatio/Elasticsearch/ElasticService.php
@@ -660,8 +660,8 @@ class ElasticService implements ElasticsearchInterface
         $queryBody = null;
         $totaltime = 0;
         $metrics = array();
-        $file = fopen("/Users/rolfguescini/source/phpelasticsearchlaudatio/storage/metrics.csv","w");
-        //$file = fopen("/var/www/html/laravelaudatio/shared/storage/metrics.csv","w");
+        //$file = fopen("/Users/rolfguescini/source/phpelasticsearchlaudatio/storage/metrics.csv","w");
+        $file = fopen("/var/www/html/laravelaudatio/shared/storage/metrics.csv","w");
         fputcsv($file,array(
             'id',
             'EStime',
