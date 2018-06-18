f4d4d6d08f1e9da8c7bf4eb73206153b9d0c3190
Rolf Guescini <rolf.guescini@hu-berlin.de>
2018-06-18 10:18:44 +0200

    adding sitecomments

--- resources/views/layouts/admin_ux.blade.php
+++ resources/views/layouts/admin_ux.blade.php
@@ -14,18 +14,24 @@
     <link href="{{ asset('css/ux_style.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/unslider.css') }}" rel="stylesheet">
     <link href="{{ asset('css/laudatio.css') }}" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('sitecomments/feedback.css') }}" />
 </head>
 
 <body>
 <div class="container-fluid m-0 p-0">
     @include ('layouts.main_ux_nav')
     @yield('content')
+    <div id="sc_feedback"></div>
+    <div id="sc_feedbackBanner"></div>
     @include('layouts.main_ux_footer')
 </div>
 <script src="{{ asset('js/vendorscripts.js') }}"></script>
 <script src="{{ asset('js/scripts.js') }}"></script>
 <script src="{{ asset('js/browseapp.js') }}"></script>
 <script src="{{ asset('js/jq.js') }}"></script>
+<script type="text/javascript" src="{{ asset('sitecomments/jquery-1.9.1.js') }}"></script>
+<script type="text/javascript">var jq191 = jQuery.noConflict(true);</script>
+<script type="text/javascript" src="{{ asset('sitecomments/feedback.js') }}"></script>
 
 @if (session('openLogin'))
     //some js function that will open your hidden modal
